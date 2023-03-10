e0260588601f5ff0db62cddc7156151c2f92f1dd
KubaOw <kuba.owczarek@icloud.com>
2023-03-10 13:44:30 +0100

    Custom migration
    Added a custom migration script

--- EntityFramework/EntityFramework/Entities/User.cs
+++ EntityFramework/EntityFramework/Entities/User.cs
@@ -3,8 +3,7 @@
     public class User
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string Lastname { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public Adress Adress { get; set; }
         public List<WorkItem> WorkItems { get; set; } = new List<WorkItem>();
---
+++ EntityFramework/EntityFramework/Migrations/20230310123835_UserFullName.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.Sql(@"
+                UPDATE Users 
+                SET FullName = FirstName + ' ' + LastName
+            ");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            //Update new columns
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Users");
+        }
+    }
+}
