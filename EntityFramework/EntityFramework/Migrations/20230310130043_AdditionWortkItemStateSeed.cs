b7ad76a6016bf08dae407502048f856783895305
KubaOw <kuba.owczarek@icloud.com>
2023-03-10 14:01:57 +0100

    Manual seed
    Added an empty migration that is filled
    manually

---
+++ EntityFramework/EntityFramework/Migrations/20230310130043_AdditionWortkItemStateSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionWortkItemStateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                column: "Value",
+                value: "On Hold");
+
+            migrationBuilder.InsertData(
+                table: "WorkItemStates",
+                column: "Value",
+                value: "Rejected");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "On Hold");
+
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "Rejected");
+        }
+    }
+}
